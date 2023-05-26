7c2c6812e8d0a7e183e75aab42a9410be1db515c
guo <2246338405@qq.com>
2023-05-26 18:14:51 +0800

    第六次提交
    完成首页侧边栏的基本样式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,33 @@
 <template>
     <div class="home">
         <el-container>
+            <!-- 头部 -->
             <el-header><el-row :gutter="20">
                     <el-col :span="4">
                         <img src="@/assets/logo.jpg" class="logo" alt="">
                     </el-col>
                     <el-col :span="16">
-                        <h2>后天管理系统</h2>
+                        <h2>后台管理系统</h2>
                     </el-col>
                     <el-col :span="4">
                         <span class="quit-login">退出登录</span>
                     </el-col>
                 </el-row></el-header>
+
+            <!-- 侧边栏 -->
             <el-container>
-                <el-aside width="200px">Aside</el-aside>
+                <el-aside width="200px">
+                    <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo"
+                        default-active="2" text-color="#fff">
+
+                        <el-menu-item index="2">
+                            <el-icon><icon-menu /></el-icon>
+                            <span>商品列表</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-aside>
+
+                <!-- 左边路由视图 -->
                 <el-main>Main</el-main>
             </el-container>
         </el-container>
@@ -35,13 +49,23 @@ export default defineComponent({
 <style lang="scss" scoped>
 .el-header {
     height: 80px;
-    background-color:#545c64;
+    background-color: #545c64;
+
     .logo {
         height: 80px;
     }
-    h2,.quit-login {
+
+    h2,
+    .quit-login {
         text-align: center;
         height: 80px;
         line-height: 80px;
     }
-}</style>
+}
+
+.el-aside {
+    .el-menu {
+        height: calc(100vh - 80px)
+    }
+}
+</style>
