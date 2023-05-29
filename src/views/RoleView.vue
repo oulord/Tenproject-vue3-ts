b8b59465af2b1a96af25b9c8562a1b8979680af9
guo <2246338405@qq.com>
2023-05-29 18:45:35 +0800

    第十七次提交
    完成角色列表页面的编写以及添加角色功能的实现

--- src/views/RoleView.vue
+++ src/views/RoleView.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        角色列表
+        <el-form :inline="true" class="search-form">
+            <el-form-item>
+                <el-button type="primary" @click="addRole">添加角色</el-button>
+            </el-form-item>
+        </el-form>
+
+        <el-table :data="list" border style="width: 100%">
+            <el-table-column prop="roleId" label="编号" width="180" />
+            <el-table-column prop="roleName" label="角色名称" width="180" />
+            <el-table-column prop="authority" label="操作">
+                <template #default="scope">
+                    <el-button link type="primary" size="small" @click="ChangeRole(scope.row)">
+                        修改权限
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, reactive, toRefs } from 'vue'
+import { ElMessage, ElMessageBox } from "element-plus";
 import { getRoleList } from '@/request/api';
 import { InitData } from '@/type/role';
 
@@ -13,16 +31,42 @@ export default defineComponent({
     name: "RoleView",
     setup() {
         const data = reactive(new InitData())
-        console.log(data);
-        
 
         onMounted(() => {
             getRoleList().then(res => {
-                console.log(res);
+                // console.log(res);
+                data.list = res.data
             })
         })
 
-        return {...toRefs(data)}
+        const addRole = () => {
+            ElMessageBox.prompt('请输入角色名称', '添加角色', {
+                confirmButtonText: '添加',
+                cancelButtonText: '取消',
+            })
+                .then(({ value }) => {   //value表示你在输入中填写的值
+                    if (value) {
+                        // 有值就添加
+                        data.list.push({
+                            roleId: data.list.length + 1,
+                            authority: [],
+                            roleName: value
+                        })
+                    }
+                    ElMessage({
+                        type: 'success',
+                        message: `${value}角色添加成功`,
+                    })
+                })
+                .catch(() => {
+                    ElMessage({
+                        type: 'info',
+                        message: '取消操作',
+                    })
+                })
+        }
+
+        return { ...toRefs(data), addRole }
     }
 })
 </script>
